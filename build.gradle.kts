b61e0676b81897f73c674331092381e1157d3826
toefel <toefel18@gmail.com>
2020-11-09 20:30:07 +0100

    Docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 
 group = "nl.toefel"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -17,11 +16,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
 	implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
@@ -36,6 +34,13 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+
+/**
+ * Fixes this error:
+ *
+ * Cannot inline bytecode built with JVM target 1.8 into bytecode that is being built with JVM target 1.6.
+ * Please specify proper '-jvm-target' option
+ */
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
